d092f22a02b960d99c5a6e2dffe38d781c07286e
Josimar Gonga <josimargonga543@gmail.com>
2026-07-26 08:54:21 +0100

    configuração do prisma

---
+++ src/workdir/infra/database/database.service.ts
@@ -0,0 +1,24 @@
+import "dotenv/config"
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from 'generated/prisma/client';
+
+@Injectable()
+export class DatabaseService extends PrismaClient implements OnModuleInit{
+
+  constructor(){
+    const adapter = new PrismaPg({ connectionString:`${process.env.DATABASE_URL}` });
+    super({adapter})
+  }
+  async onModuleInit() {
+
+    try {
+      await this.$connect
+      Logger.log("database conectado")
+    } catch (error) {
+      Logger.log("database conectado", error)
+    }
+  }
+  
+
+}
